c2862b0408f18d0916a5506cf150927f3d31d231
Thom Dickson <td3of4@gmail.com>
2023-12-07 12:23:29 -0500

    Write main logic loop in simulator

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,100 +1,116 @@
 package simulator;
+
 import java.io.*;
 import java.util.*;
 import tsgui.*;
+import simulator.FuncionalUnit;
+import simulator.CDB;
 
-public class Simulator
-{
-    //both of these are for the GUI 
+public class Simulator {
+    // both of these are for the GUI
     boolean gui;
     private ArrayList<CycleSnapShot> snapshots = null;
 
-
-    public Simulator(String file, boolean flag) throws IOException
-    {
-        //create your functional units in here
+    public Simulator(String file, boolean flag) throws IOException {
+        // create your functional units in here
         System.out.println("In constructor!");
         gui = flag;
     }
 
-    public void simulate()
-    {
-        //initialize variables passed to gui
+    public void simulate() {
+        // initialize variables passed to gui
         long cdbResult = 0;
         String cdbSrc = "none";
         int PC = 0;
         int instruction = 0;
 
         boolean halt = false;
-        //this will be your driver loop which will execute until
-        //the halt is executed
-	// TODO: Instantiate CDB
-	// TODO: Instiate FUs 
-	// TODO: Detect when stall is called for, 
-	// that is, when no functional unit was able to accept the instruction
-	CDB cdb = CDB.getInstance();
-        while (halt == false)
-        {
-	    boolean stall = true;
-	    // ISSUE
-	    // for (FU fu : FUS) {
-	    // 	stall =  stall && fu.tryIssueInstruction(instruction)
-	    // }
-	    // EXECUTE
-	    // TODO: Check FUs for values to write on the CDB, but only one
-	    // for (FU fu : FUS) {
-	    // 	fu.execute(instruction, cdb, ...)
-	    // }
-	    //
-	    // 
-	    //
-	    // WRITEBACK
-	    // for (FU fu : FUS) {
-	    // 	fu.tryWrite(instruction, cdb, ...)
-	    // }
+        // this will be your driver loop which will execute until
+        // the halt is executed
 
-	    if (!stall) {
-		// TODO: Get next instruction 
-	    }
+        // Instiate FUs
+        FuncionalUnit[] functionalUnits = {
+                new IntegerUnit(),
+                new BranchUnit(),
+                new MemoryUnit(),
+                new FPAddUnit(),
+                new FPMultUnit(),
+                new FPDivUnit(),
+        };
 
-	    cdb.busy = false;
-            if (gui) addSnapShot(instruction, PC, cdbResult, cdbSrc);
+        // TODO: Detect when stall is called for,
+        // that is, when no functional unit was able to accept the instruction
+        CDB cdb = CDB.getInstance();
+        while (halt == false) {
+            boolean stall = true;
+
+            // ISSUE //
+            for (FuncionalUnit fu : functionalUnits) {
+                stall = stall && fu.tryIssueInstruction(instruction);
+                // TODO: At this point, we need to check if the instruction issues was a halt.
+                // When we issue a halt, we need to stop issueing instrucdtions and wait for the
+                // reservation stations to empty out.
+            }
+
+            // EXECUTE //
+            // Reserveration stations need to pull in new data before we execute
+            for (FuncionalUnit fu : functionalUnits) {
+                fu.updateReservationStations(cdb);
+            }
+
+            // Actually execute a single cycle
+            for (FuncionalUnit fu : functionalUnits) {
+                fu.execute();
+            }
+
+            // WRITEBACK //
+            for (FuncionalUnit fu : functionalUnits) {
+                fu.cdbWrite();
+            }
+
+            if (!stall) {
+                // TODO: Get next instruction
+                PC++;
+            }
+
+            cdb.busy = false;
+            if (gui)
+                addSnapShot(instruction, PC, cdbResult, cdbSrc);
         }
-        if (gui == true) new TSGui(snapshots);
+        if (gui == true)
+            new TSGui(snapshots);
     }
-	
-    //This method is for the GUI, do not modify this
-    public void addSnapShot(int instr, int PCValue, 
-                            long cdbValue, String cdbSrc)
-    {
-         if (snapshots == null) snapshots = new ArrayList<CycleSnapShot>();
-          
-          snapshots.add(new CycleSnapShot(Clock.getInstance().get(), instr,
-                        PCValue, buildFunctionalUnitImageList(), cdbValue,
-                        cdbSrc));
-     }
 
-     //You'll need to modify this method to use the GUI
-     private ArrayList<FUnitImage> buildFunctionalUnitImageList()
-     {
-          ArrayList<FUnitImage> list = new ArrayList<FUnitImage>();
+    // This method is for the GUI, do not modify this
+    public void addSnapShot(int instr, int PCValue,
+            long cdbValue, String cdbSrc) {
+        if (snapshots == null)
+            snapshots = new ArrayList<CycleSnapShot>();
 
-          //you'll need to create a new FUnitImage object for each
-          //of your Functional Units and pass that object to list.add
-          //Each Functional Unit will have an array of Stations,
-          //a count of the Stations, an executionCount, the currentInstruction,
-          //a flag indicating whether it is busy or not, and the
-          //number of remaining execution Cycles
-          //
-          //Here is a sample call, although it won't work until there
-          //is an intUnit object.
-/*
-          list.add(new FUnitImage("integer", intUnit.RS, intUnit.RScount,
-                   intUnit.executionCount, intUnit.currentInstruction,
-                   intUnit.FUbusy, intUnit.executionCycles));
+        snapshots.add(new CycleSnapShot(Clock.getInstance().get(), instr,
+                PCValue, buildFunctionalUnitImageList(), cdbValue,
+                cdbSrc));
+    }
 
-*/
-          return list;
-     }
+    // You'll need to modify this method to use the GUI
+    private ArrayList<FUnitImage> buildFunctionalUnitImageList() {
+        ArrayList<FUnitImage> list = new ArrayList<FUnitImage>();
+
+        // you'll need to create a new FUnitImage object for each
+        // of your Functional Units and pass that object to list.add
+        // Each Functional Unit will have an array of Stations,
+        // a count of the Stations, an executionCount, the currentInstruction,
+        // a flag indicating whether it is busy or not, and the
+        // number of remaining execution Cycles
+        //
+        // Here is a sample call, although it won't work until there
+        // is an intUnit object.
+        /*
+         * list.add(new FUnitImage("integer", intUnit.RS, intUnit.RScount,
+         * intUnit.executionCount, intUnit.currentInstruction,
+         * intUnit.FUbusy, intUnit.executionCycles));
+         * 
+         */
+        return list;
+    }
 }
-
