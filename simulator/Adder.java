943b781c2063e7e5608c270cdedab38491ca8a49
Phillip Davis <phdavis1027@gmail.com>
2023-12-07 11:26:51 -0500

    add package declarations

--- simulator/Adder.java
+++ simulator/Adder.java
@@ -1,3 +1,5 @@
+package simulator; 
+
 public class Adder extends FunctionalUnit {
 	@Override
 	public long computeResult(Station station) {
--- simulator/CDB.java
+++ simulator/CDB.java
@@ -1,3 +1,5 @@
+package simulator;
+
 public class CDB {
 	long data;
 	String source;
